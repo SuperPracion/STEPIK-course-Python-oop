080fe0119e38622dc1824333203e7d1f04d00e98
SuperPracion <pracion@bk.ru>
2023-01-27 17:46:47 +0300

    Выполнение модуля 2.5

---
+++ Section 2.5/2_out_privete_data.py
@@ -0,0 +1,12 @@
+"""
+class PizzaMaker:
+    def __make_pepperoni(self):
+         ...
+
+    def _make_barbecue(self):
+         ...
+"""
+
+maker = PizzaMaker()
+maker._make_barbecue()
+maker._PizzaMaker__make_pepperoni()
