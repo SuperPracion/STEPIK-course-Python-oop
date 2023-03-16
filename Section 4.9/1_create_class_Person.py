f478c92ab782e5cf1d858e0910b17229bee016e9
SuperPracion <pracion@bk.ru>
2023-03-16 20:48:51 +0300

    Выполнение модуля 4.9

---
+++ Section 4.9/1_create_class_Person.py
@@ -0,0 +1,10 @@
+class Person:
+    __slots__ = ('first_name', 'last_name', 'age')
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} is {self.age} years old'
